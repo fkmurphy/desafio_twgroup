83f3b12091776ed7affcc73b368aa4f01c59bd88
Julian Murphy <njmdistrisoft@gmail.com>
2020-08-13 17:24:57 -0300

    models

---
+++ app/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = 'Comment';
+
+    protected $fillable = [
+        'status', 'content'
+    ];
+
+    protected $guarded = [
+        'publication_id'
+    ];
+
+    public function publication(){
+        return $this->belongsTo(Publication::class,'id','publication_id');
+    }
+}
---
+++ app/Publication.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Publication extends Model
+{
+    protected $table = 'documents';
+
+    protected $fillable = [
+        'title', 'content'
+    ];
+
+    protected $guarded = [
+        'user_id'
+    ];
+
+    public function comments(){
+        return $this->hasMany(Comment::class,'publication_id','id');
+    }
+    public function user(){
+        return $this->hasOne(User::class,'id','user_id');
+    }
+   
+}
